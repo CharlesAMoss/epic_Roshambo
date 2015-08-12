1c4ec91a7566703a2a500261e57b40793a880805
Dana Sharman & Charles Moss <student@epicodus.com>
2015-08-12 16:54:51 -0700

    end of day

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -2,19 +2,20 @@
 class RockPaperScissors
 {
 
+    function RPSgame($play1, $play2)
+    {
+        $output = array();
+        $output = array($play1, $play2);
 
+        $game_result = array();
+        $game_result = array('the game winner');
 
+        //$output = array_push($output, $game_result);
 
+        var_dump($output);
 
-
-  function RPSgame($play1,$play2)
-  {
-      $output = array($play1, $play2);
-
-      $game_result = "the game winner";              
-
-      return $output;
-  }
+        return $output;
+    }
 
 
 
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -6,13 +6,14 @@
     {
 
         // player inputs a string and game outputs a array. input:"string" "string" output: "[]"
-        function test_RockPaperScissors_returnArray()
+       function test_RockPaperScissors_returnArray()
         {
 
             //Arrange
             $test_RockPaperScissors = new RockPaperScissors;
             $input1 = "foo";
             $input2 = "bar";
+
 
             //Act
             $result = $test_RockPaperScissors->RPSgame($input1, $input2);
@@ -39,4 +40,20 @@
 
         }//end test
 
+/*        function test_RockPaperScissors_testOutputResult()
+        {
+
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "foo";
+            $input2 = "bar";
+
+            //Act
+            $result = $test_RockPaperScissors->RPSgame($input1, $input2);
+
+            //Assert
+            $this->assertContains($input1, $result);
+
+        }//end test
+*/
     }
